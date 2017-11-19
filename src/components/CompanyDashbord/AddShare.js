2d6a61e4479cb1bb4ab15ac4b115af8ed30c3454
ghulam mustafa raza qadri <1q2w3e.gm@gmail.com>
2017-11-19 12:03:26 +0500

    Adding Tabs

--- src/components/CompanyDashbord/AddShare.js
+++ src/components/CompanyDashbord/AddShare.js
@@ -1,11 +1,95 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Radium from 'radium';
+import { Grid, Row, Col } from 'react-bootstrap'
+import globalConst from '../../globalConst';
+import Title from '../../components/Title'
+import FieldGroup from '../../components/FieldGroup'
+import { connect } from 'react-redux';
+import { signup } from '../../store/actions/authActions';
+import Button from '../../components/Button'
+import { database, auth } from 'firebase'
 
-export default class AddShare extends Component {
+var styles = {
+
+
+};
+
+class Signup extends React.Component {
+  constructor() {
+    super();
+    this.state = { investment: '', perShare: '' }
+    this.signup = this.signup.bind(this)
+  }
+
+  signup(e) {
+    e.preventDefault();
+    let { investment, perShare } = this.state
+    var payload = { investment, perShare, totalShare: investment / perShare }
+    console.log(payload)
+    // auth().signInWithEmailAndPassword('a@a.com', '123456').then(a => {
+    this.props.AddShare(payload)
+    // })
+  }
+  onChange = (a) => (b) => this.setState({ [a]: b.target.value })
   render() {
     return (
-      <div>
-        Add Share
-      </div>
-    )
+      <Grid>
+        <Row className="show-grid">
+          <Col xs={12} >
+            <Title>Add Share</Title>
+          </Col>
+          <form>
+            <Col xs={12} mdOffset={3} md={6}>
+              <FieldGroup
+                type="number"
+                label="Investment"
+                placeholder="Investment"
+                value={this.state.investment}
+                onChange={this.onChange('investment')}
+              />
+              <FieldGroup
+                type="number"
+                label="PerShare"
+                placeholder="PerShare"
+                value={this.state.perShare}
+                onChange={this.onChange('perShare')}
+              />
+            </Col>
+            <Col xs={12} style={{ textAlign: 'center', marginBottom: 50 }}>
+              <Button filled clickHandler={this.signup}>
+                Add Share
+              </Button>
+            </Col>
+          </form>
+        </Row>
+      </Grid>
+
+    );
   }
 }
+
+
+
+Signup = Radium(Signup);
+
+const mapStateToProps = state => {
+  return {
+    user: state.authReducer
+  }
+}
+let addShare = (dispatch, payload) => {
+  auth().onAuthStateChanged(user => {
+    database().ref('Shares/' + user.uid).push(payload).then(() => {
+      console.log('hogaya')
+    })
+  })
+  // dispatch({ type: '', payload })
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    AddShare: (payload) => {
+      addShare(dispatch, payload)
+    }
+  }
+}
+export default connect(mapDispatchToProps, mapDispatchToProps)(Signup)
--- src/components/CompanyDashbord/MyShare.js
+++ src/components/CompanyDashbord/MyShare.js
@@ -1,11 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Radium from 'radium';
+import { Grid, Row, Col } from 'react-bootstrap'
+import globalConst from '../../globalConst';
+import Title from '../../components/Title'
+import FieldGroup from '../../components/FieldGroup'
+import { connect } from 'react-redux';
+import { signup } from '../../store/actions/authActions';
+import Button from '../../components/Button'
+import { database, auth } from 'firebase'
 
-export default class MayShare extends Component {
+var styles = {
+};
+
+class Signup extends React.Component {
+  constructor() {
+    super();
+    this.state = { shares: [] }
+  }
+  componentDidMount() {
+    
+  }
   render() {
     return (
       <div>
-        MayShare
+
       </div>
-    )
+    );
   }
 }
+
+Signup = Radium(Signup);
+
+const mapStateToProps = state => {
+  return {
+    shares: state.shareReducer
+  }
+}
+
+let fetchShares = (dispatch, payload) => {
+  auth().onAuthStateChanged(user => {
+    database().ref('Shares/' + user.uid).off()
+    database().ref('Shares/' + user.uid).on('value', snap => {
+      dispatch({ type: 'SET_SHARE', payload: Object.values(snap.val()) })
+    })
+  })
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchShares: (payload) => {
+      fetchShares(dispatch, payload)
+    }
+  }
+}
+
+export default connect(mapDispatchToProps, mapDispatchToProps)(Signup)
